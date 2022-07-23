8e87ef336f4cb4ce1d42ef7a6104eb83fdac7c18
Wafa <wafa.trabelsi@esprit.tn>
2022-07-23 14:04:53 +0200

    wafafinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,32 +11,21 @@ import {ListAgentComponent} from "./list-agent/list-agent.component";
 import {AddEditAgentComponent} from "./add-edit-agent/add-edit-agent.component";
 import {MesComptesComponent} from "./mes-comptes/mes-comptes.component";
 import {AllComptesComponent} from "./all-comptes/all-comptes.component";
-import { ChangeComponent } from './change/change.component';
-import { CarnetchequeComponent } from './carnetcheque/carnetcheque.component';
-import { ChequeAdminComponent } from './cheque-admin/cheque-admin.component';
-import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   //{ path: '', component: HomeComponent},
-  {path:'',component:LoginComponent},
-  {path:'wafa',component:LayaoutComponent, canActivate: [AuthGuard], children: [
-      {path: '', component: ListDemandeComponent},
+  {path:'',component:LayaoutComponent, canActivate: [AuthGuard], children: [
+      {path: 'listDemande', component: ListDemandeComponent},
       {path: 'detailDemande/:id', component: DetailDemandeCompteComponent},
       {path: 'listAgent', component: ListAgentComponent},
       {path: 'agent/new', component: AddEditAgentComponent},
       {path: 'agent/edit/:id', component: AddEditAgentComponent},
       {path: 'mesComptes', component: MesComptesComponent},
       {path: 'allAccount', component: AllComptesComponent},
-      {path:'change/:idcompte',component:ChangeComponent},
-      {path:'carnetcheque/:idcompte',component:CarnetchequeComponent},
-      {path:'PaymentChequeAdmin',component:ChequeAdminComponent},
-    ]},
-  // {path:'change/:idcompte',component:ChangeComponent},
-  // {path:'carnetcheque/:idcompte',component:CarnetchequeComponent},
-  // {path:'PaymentChequeAdmin',component:ChequeAdminComponent},
-  
 
+    ]},
+  {path:'login',component:LoginComponent},
   {path:'demandeCompte',component:DemandeCompteComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DeviseComponent } from './devise/devise.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
@@ -14,7 +13,7 @@ import { DemandeCompteComponent } from './demande-compte/demande-compte.componen
 import { PieceJointeComponent } from './piece-jointe/piece-jointe.component';
 import { LayaoutComponent } from './layaout/layaout.component';
 import { LoginComponent } from './login/login.component';
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { ListDemandeComponent } from './list-demande/list-demande.component';
@@ -25,9 +24,6 @@ import { ListAgentComponent } from './list-agent/list-agent.component';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { MesComptesComponent } from './mes-comptes/mes-comptes.component';
 import { AllComptesComponent } from './all-comptes/all-comptes.component';
-import { ChangeComponent } from './change/change.component';
-import { CarnetchequeComponent } from './carnetcheque/carnetcheque.component';
-import { ChequeAdminComponent } from './cheque-admin/cheque-admin.component';
 
 
 @NgModule({
@@ -48,11 +44,7 @@ import { ChequeAdminComponent } from './cheque-admin/cheque-admin.component';
     AddEditAgentComponent,
     ListAgentComponent,
     MesComptesComponent,
-    AllComptesComponent,
-    DeviseComponent,
-    ChangeComponent,
-    CarnetchequeComponent,
-    ChequeAdminComponent
+    AllComptesComponent
 
     ],
   imports: [
